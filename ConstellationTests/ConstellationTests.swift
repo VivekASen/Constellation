afdb9a48b256da9ca56f28ac7812925c396e2966
Vivek <sen14vivek@gmail.com>
2026-02-25 23:09:29 -0500

    Initial Constellation Project

---
+++ ConstellationTests/ConstellationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConstellationTests.swift
+//  ConstellationTests
+//
+//  Created by Vivek  Sen on 2/25/26.
+//
+
+import Testing
+@testable import Constellation
+
+struct ConstellationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
